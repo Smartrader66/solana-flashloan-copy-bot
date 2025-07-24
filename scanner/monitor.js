41a8a81b15ada583ac688633d0bddb767565a203
Smartrader66 <trobion@proton.me>
2025-07-24 17:50:07 +0200

    Create monitor.js

---
+++ scanner/monitor.js
@@ -0,0 +1,6 @@
+// scanner/monitor.js
+
+module.exports = async function monitorSlots() {
+    console.log("[Monitor] Slot monitoring initialized (placeholder)");
+    // Hier wird später die Rückwärts-Slot-Analyse eingebaut
+};
