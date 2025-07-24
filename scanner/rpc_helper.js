ccb4fb9a30f55ffbc1bd330df805d82075bda743
Smartrader66 <trobion@proton.me>
2025-07-24 17:58:25 +0200

    Create rpc_helper.js

---
+++ scanner/rpc_helper.js
@@ -0,0 +1,9 @@
+// scanner/rpc_helper.js
+//
+// Placeholder module for Solana RPC communication helpers.
+// This file will contain logic for interacting with Solana RPC endpoints,
+// including block data retrieval and transaction filtering.
+
+module.exports = {
+  // Future implementation will go here
+};
